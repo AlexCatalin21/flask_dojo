6e1bf9e8989d8a715493f6b2fb1a1c1c4b7b85ec
AlexCatalin21 <alexandru.g2101@gmail.com>
2020-02-10 15:08:41 +0200

    put and delete added

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 get_counter = 0
 post_counter = 0
+put_counter =0
+delete_counter=0
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -11,23 +13,28 @@ def main_page():
     return  render_template("index.html")
 
 
-@app.route("/request_counter", methods=["GET", "POST"])
+@app.route("/request_counter", methods=["GET", "POST", "PUT", "DELETE"])
 def request_counter():
     global post_counter
     global get_counter
+    global put_counter
+    global delete_counter
     if request.method == "POST":
         post_counter+=1
     elif request.method == "GET":
         get_counter+=1
+    elif request.method == "PUT":
+        put_counter += 1
+    elif request.method == "DELETE":
+        delete_counter += 1
+
     return render_template("index.html")
 
-@app.route("/statistics", methods=["GET","POST"])
+@app.route("/statistics", methods=["GET","POST","PUT", "DELETE"])
 def statistics():
-    return render_template("statistics.html", get_counter=get_counter, post_counter=post_counter)
+    return render_template("statistics.html", get_counter=get_counter, post_counter=post_counter, put_counter=put_counter, delete_counter=delete_counter)
 
 if __name__ == "__main__":
     app.run(
-        host='0.0.0.0',
-        port=8000,
-        debug=True,
+        debug=True
     )
